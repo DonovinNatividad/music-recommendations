53cdd32d24dff30d48c16565171bda6266516273
DonovinNatividad <Donovincall@gmail.com>
2024-09-29 15:13:16 -0400

    Created login route to allow users to authenticate in my application to allow them to use the api routes

--- backend/app/api/recommendations.py
+++ backend/app/api/recommendations.py
@@ -2,6 +2,17 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 import requests
 from utils.token_manager import token_manager
+from dotenv import load_dotenv
+import os
+
+# These are all imported for the login library 
+from fastapi.responses import RedirectResponse, JSONResponse
+import random
+import string
+import urllib.parse
+
+# Load environment variables from .env file
+load_dotenv()
 
 api = APIRouter()
 
@@ -9,8 +20,48 @@ class RecommendationRequest(BaseModel):
     seed_tracks: list[str]
 
 def get_access_token():
-    token = token_manager.get_token()
+    token = token_manager.token
     return {'Authorization': f'Bearer {token}'}
+
+def generate_random_string(length: int):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+@api.get('/login')
+def login():
+    state = generate_random_string(16)
+    scope = 'user-read-private user-read-email'
+    
+    client_id = os.getenv('SPOTIFY_CLIENT_ID')
+    redirect_uri = os.getenv('REDIRECT_URI').strip()
+    
+    query_params = {
+        'response_type': 'code',
+        'client_id': client_id,
+        'scope': scope,
+        'redirect_uri': redirect_uri,
+        'state': state
+    }
+    
+    url = 'https://accounts.spotify.com/authorize?' + urllib.parse.urlencode(query_params)
+    return RedirectResponse(url)
+
+@api.get('/callback')
+def callback(code: str, state: str):
+    # Check if code and state are received properly
+    print(f"Received code: {code}, state: {state}")
+    
+    # Grab the success URI
+    redirect_uri = os.getenv('REDIRECT_URI').strip()
+
+    # Validate that you received a valid code and state
+    if not code or not state:
+        return JSONResponse(content={"error": "Missing code or state"}, status_code=400)
+
+    # Exchange the authorization code for a token
+    user_token = token_manager.get_user_token(code, redirect_uri)
+    
+    return JSONResponse(content={'message': 'Authorization successful', 'token': user_token}, status_code=200)
+
 
 @api.post('/recommendations')
 def get_recommendations(request: RecommendationRequest):
